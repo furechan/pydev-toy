4b9067632db8542a6c570fdf046cf23603f3feb4
Furechan <furechan@xsmail.com>
2025-11-25 07:37:17 -0500

    ...

--- tasks.py
+++ tasks.py
@@ -1,57 +1,67 @@
-# noinspection PyUnresolvedReferences
+# ---- noinspection PyUnresolvedReferences
 
-import re
 
 from pathlib import Path
-from invoke import task  # type: ignore
 
-ROOT = Path(__file__).parent
+from packaging.version import Version
+
+from invoke import task  # --- type: ignore
+
+
+ROOTDIR = Path(__file__).parent
+
+
+def bump_version(version):
+    """Bump patch version"""
+    if isinstance(version, str):
+        version = Version(version)
+    return f"{version.major}.{version.minor}.{version.micro + 1}"
+
 
 
 @task
-def clean(ctx):
+def pwd(c):
+    """Print working directory"""
+    c.run("pwd")
+
+
+
+@task
+def clean(c):
     """Clean project dist"""
-    ctx.run("rm -rf dist")
+    c.run("rm -f dist/*")
+
 
 
 @task
-def check(ctx):
-    """Check package"""
-    ctx.run("ruff check")
+def dump(c):
+    """Dump wheel contents"""
+    for file in ROOTDIR.glob("dist/*.whl"):
+        c.run(f"unzip -l {file}")
 
 
 @task(clean)
-def build(ctx):
+def build(c):
     """Build project wheel"""
-    ctx.run("python -mbuild --wheel")
+    c.run("uv build --wheel")
 
 
-@task
-def dump(ctx):
-    """Dump wheel contents"""
-    for file in ROOT.glob("dist/*.whl"):
-        ctx.run(f"unzip -l {file}")
 
+@task(clean)
+def publish(c):
+    """Build and publish package wheel"""
 
-@task
-def publish(ctx, testpypi=False):
-    """Publish to PyPI with twine"""
-    flags = "--repository testpypi" if testpypi else ""
-    ctx.run(f"twine upload {flags} dist/*.whl")
+    with c.cd(ROOTDIR):
+        version = Version(c.run("uv version --short", hide=True).stdout)
+        print(version, version.base_version)
 
+        if version.is_prerelease:
+            c.run("uv version --bump stable")
 
-@task
-def bump(ctx):
-    """Bump patch version in pyproject"""
-    pyproject = ROOT.joinpath("pyproject.toml").resolve(strict=True)
-    buffer = pyproject.read_text()
-    pattern = re.compile(r"(?mx)^version \s* = \s* \"(.+)\" \s*")
-    match = pattern.search(buffer)
-    if not match:
-        raise ValueError("Could not find version setting")
-    version = tuple(int(i) for i in match.group(1).split("."))
-    version = version[:-1] + (version[-1] + 1,)
-    version = ".".join(str(v) for v in version)
-    print(f"Updating version to {version} ...")
-    output = pattern.sub(f'version = "{version}"\n', buffer)
-    pyproject.write_text(output)
+        c.run("uv build --wheel")
+        c.run("uv publish")
+
+        version = bump_version(version) + ".dev0"
+
+        c.run(f"uv version {version}")
+
