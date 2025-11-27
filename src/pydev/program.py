7aba2036c5bc760e0bc6698d8486ac98418806dc
Furechan <furechan@xsmail.com>
2025-11-27 10:17:13 -0500

    ...

--- src/pydev/program.py
+++ src/pydev/program.py
@@ -173,7 +173,7 @@ def release(ctx, test_pypi=False, verbose=False):
         utils.update_config("project.version", version)
 
     if git_found:
-        message = f"Release version {version}"
+        message = f"Release {version}"
         utils.run_command(f"git commit -am '{message}'", cwd=root)
 
     ctx.invoke(clean)
